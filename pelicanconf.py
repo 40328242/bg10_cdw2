37447901c09235e4b0bd5711246e3e760e60faa7
kmol2016spring <kmol2016spring@example.com>
2016-05-13 18:12:08 +0800

    0513

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'kmol'
 SITENAME = 'CDW2 網頁 (虎尾科大MDE)'
-SITEURL = 'http://40323250.github.io/bg9_cdw2/'
+SITEURL = 'http://40328242.github.io/bg10_cdw2/'
 
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
